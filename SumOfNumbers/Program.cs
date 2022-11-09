7ba2c19daf5af9e65732c35db2609c7104082cb0
haievyiivan <haievyiivan@gmail.com>
2022-11-09 10:38:05 +0200

    Add project files.

---
+++ SumOfNumbers/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int sum = 0; // Create variable to contain sum num
+
+            // Loop
+            for (int i = 1; i <= 10/*Num of numbers*/; i++)
+            {
+                Console.WriteLine(i); // Write num to the console
+
+                sum += i; // Add num to sum variable
+            }
+
+            Console.WriteLine($"Sum = {sum}"); // Write sum to the console
+
+            // ReadKey
+            Console.ReadKey();
+        }
+    }
+}
